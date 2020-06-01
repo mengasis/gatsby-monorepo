08f52ef35076d0d97673ff311ce9a554fee5653d
Manuel <mengasis@outlook.com>
2020-06-01 16:17:59 -0400

    fix: 🐛 update releaserc

---
+++ .releaserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/git",
+            {
+                assets: ["dist/**/*.{js,css}", "docs", "package.json"],
+                message:
+                    "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            },
+        ],
+    ],
+    branches: [
+        "+([0-9])?(.{+([0-9]),x}).x",
+        "master",
+        "next",
+        "next-major",
+        { name: "beta", prerelease: true },
+        { name: "alpha", prerelease: true },
+    ],
+};
