8a0b07361260588bb2857d81886ba30fa8c30651
Manuel <mengasis@outlook.com>
2020-06-01 16:21:26 -0400

    fix: 🐛 added git plugin to semantic-release

--- .releaserc.js
+++ .releaserc.js
@@ -10,6 +10,10 @@ module.exports = {
                     "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
             },
         ],
+        ["@semantic-release/git", {
+            "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }]
     ],
     branches: [
         "+([0-9])?(.{+([0-9]),x}).x",
