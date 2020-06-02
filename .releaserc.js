94f70a1245794b18629e62adcc1f0cdae78d8863
Manuel <mengasis@outlook.com>
2020-06-01 23:38:56 -0400

    fix(blog): update title

--- .releaserc.js
+++ .releaserc.js
@@ -1,20 +1,8 @@
 module.exports = {
+    extends: "semantic-release-monorepo",
     plugins: [
         "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        [
-            "@semantic-release/git",
-            {
-                assets: ["dist/**/*.{js,css}", "docs", "package.json"],
-                message:
-                    "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-            },
-        ],
-        ["@semantic-release/git", {
-            "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        }]
-    ],
+        "@semantic-release/release-notes-generator"],
     branches: [
         "+([0-9])?(.{+([0-9]),x}).x",
         "master",
--- packages/blog/gatsby-config.js
+++ packages/blog/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Ecommerce',
+    title: 'Ecommerce Blog',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
